4c5212d2ba5bb68b01bd9b17d97149fc1b7dcb2c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-24 23:51:38 +0000

    strace: add 4.15 bottle for Linuxbrew.

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -5,6 +5,11 @@ class Strace < Formula
   url "https://github.com/strace/strace/archive/v4.15.tar.gz"
   sha256 "e5fc45bdf72a36e621868ff4c67d645a5087d3d2f31571ad6bca21c64b5ac870"
   head "https://github.com/strace/strace.git"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c1665cc4585a2788b10b5ed6c5712f8cb3833d543e81699b2c946ddf0ef92783" => :x86_64_linux
+  end
+
   # tag "linuxbrew"
 
   depends_on "linux-headers"
