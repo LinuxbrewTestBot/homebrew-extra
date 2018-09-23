55131c98d4dac6965367d5f8d6df68712df1da37
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-23 09:05:08 +0000

    libunwind: add 1.2.1 bottle.

--- Formula/libunwind.rb
+++ Formula/libunwind.rb
@@ -4,6 +4,12 @@ class Libunwind < Formula
   url "https://download.savannah.nongnu.org/releases/libunwind/libunwind-1.2.1.tar.gz"
   sha256 "3f3ecb90e28cbe53fba7a4a27ccce7aad188d3210bb1964a923a731a27a75acb"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-extra"
+    cellar :any_skip_relocation
+    sha256 "2ce6b428f17ecbdef505e442dc26900cd20bbac936536781306378ea353461c0" => :x86_64_linux
+  end
+
   head do
     url "https://github.com/libunwind/libunwind.git"
     depends_on "autoconf" => :build
